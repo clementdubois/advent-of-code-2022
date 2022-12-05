c28b50f946f5774727679eb2a827f1b3f004b923
Clément Dubois <clement.dubois@octo.com>
2022-12-05 19:49:04 +0100

    [Day-1][Part 2] : SRP class Calorie Counter (GPT chat)

--- day-1-calorie-counting/src/CalorieCounter.ts
+++ day-1-calorie-counting/src/CalorieCounter.ts
@@ -1,32 +1,49 @@
 export type FoodCaloriesPerElf = number[][];
 
 export class CaloriesCounter {
+    private elvesToCountCalculator: ElvesToCountCalculator;
+    private caloriesCalculator: CaloriesCalculator;
     constructor() {
+        this.elvesToCountCalculator = new ElvesToCountCalculator()
+        this.caloriesCalculator = new CaloriesCalculator()
     }
 
-    countMaxCalories(caloriesPerElves: FoodCaloriesPerElf, options: { numberOfElves: number } = {numberOfElves: 1}): number {
+    countMaxCalories(caloriesPerElves: FoodCaloriesPerElf, options?: CalorieCounterOptions): number {
         const totalCaloriesPerElf = this.getTotalCaloriesPerElf(caloriesPerElves);
-        return CaloriesCounter.getMaxCalories(totalCaloriesPerElf, options.numberOfElves)
+        const elvesToCount = this.elvesToCountCalculator.getElvesToCount(totalCaloriesPerElf, options);
+        return this.caloriesCalculator.sumTotalCaloriesForElves(elvesToCount);
     }
 
     private getTotalCaloriesPerElf(caloriesPerElves: FoodCaloriesPerElf) {
         return caloriesPerElves.map((elfFoodCalories) => CaloriesCounter.getTotalCaloriesForOneElf(elfFoodCalories));
     }
 
-    private static getMaxCalories(totalCaloriesPerElf: number[], numberOfElvesToCount = 1) {
-        const sortedTotalCaloriesPerElf = this.sortTotalCaloriesByElf(totalCaloriesPerElf)
-        return this.sumTotalCaloriesForNumberOfElvesToCount(sortedTotalCaloriesPerElf, numberOfElvesToCount);
-    }
-
-    private static sumTotalCaloriesForNumberOfElvesToCount(sortedTotalCaloriesPerElf: number[], numberOfElvesToCount: number) {
-        return sortedTotalCaloriesPerElf.slice(0, numberOfElvesToCount).reduce((totalCalories, currentCalories) => totalCalories + currentCalories, 0);
-    }
-
-    private static sortTotalCaloriesByElf(totalCaloriesPerElf: number[]) {
-        return totalCaloriesPerElf.slice().sort((a, b) => b - a);
-    }
-
     private static getTotalCaloriesForOneElf(elfFoodCalories: number[]) {
         return elfFoodCalories.reduce((totalCalories, currentCalory) => totalCalories + currentCalory, 0);
+    }
+}
+
+class CalorieCounterOptions {
+    public numberOfElves: number = 1;
+
+    constructor(options?: { numberOfElves: number } ) {
+        this.numberOfElves = options?.numberOfElves || 1;
+    }
+}
+
+class ElvesToCountCalculator {
+    public getElvesToCount(totalCaloriesPerElf: number[],  options: { numberOfElves: number } = {numberOfElves: 1}): number[] {
+        const sortedTotalCaloriesPerElf = this.sortTotalCaloriesByElf(totalCaloriesPerElf);
+        return sortedTotalCaloriesPerElf.slice(0, options.numberOfElves);
+    }
+
+    private sortTotalCaloriesByElf(totalCaloriesPerElf: number[]): number[] {
+        return totalCaloriesPerElf.slice().sort((a, b) => b - a);
+    }
+}
+
+class CaloriesCalculator {
+    public sumTotalCaloriesForElves(elvesToCount: number[]): number {
+        return elvesToCount.reduce((totalCalories, currentCalories) => totalCalories + currentCalories, 0);
     }
 }
