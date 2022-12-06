a58aeb5f6ca358eb5b9fd0e53d289b17a48d080d
Clément Dubois <clement.dubois@octo.com>
2022-12-06 17:44:27 +0100

    [DAY 2] - outcome calculator loss

--- day-2/RPSScoreCalculator.test.ts
+++ day-2/RPSScoreCalculator.test.ts
@@ -1,8 +1,10 @@
 enum SHAPE_OPPONENT {
-    ROCK = "A"
+    ROCK = "A",
+    PAPER = "B"
 }
 
 enum SHAPE_ME {
+    ROCK = "X",
     PAPER = "Y"
 }
 
@@ -12,7 +14,30 @@ class RPSScoreCalculator {
     }
 }
 
+class ScoreOutcomeCalculator {
+    count() {
+        return 0
+    }
+}
+
+/* Questions qui me viennent :
+*  - Avec cette démarche j'implémente deux règles en même temps ( calcul du score de la mache = point de victoire + ce qu'on a joué),
+* est ce qu'il faut décomposé sachant que d'un point de vue externe seul nous interesse le total ?
+* => Ma solution, je créer des classe intermédiaire que je tests, pas sur que ça soit ok
+* */
 describe("RPSScoreCalculator", () => {
+    describe("ScoreOutcomeCalculator", () => {
+        test("Should return 0 if I lose", () => {
+            // GIVEN
+            const rounds = [[SHAPE_OPPONENT.PAPER, SHAPE_ME.ROCK]]
+            let scoreOutcomeCalculator = new ScoreOutcomeCalculator();
+
+            // WHEN
+            const outcomeScore = scoreOutcomeCalculator.count();
+            // THEN
+            expect(outcomeScore).toEqual(0)
+        })
+    })
     test("Should return 8 for A - Y / Rock - Paper", () => {
         // GIVEN
         const rounds = [[SHAPE_OPPONENT.ROCK, SHAPE_ME.PAPER]]
