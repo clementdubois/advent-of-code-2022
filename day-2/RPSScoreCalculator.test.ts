f0ef8c0e59720032fe5e107db2c457e3cc05848b
Clément Dubois <clement.dubois@octo.com>
2022-12-06 20:10:23 +0100

    [DAY 2] - add all case test for loss

--- day-2/RPSScoreCalculator.test.ts
+++ day-2/RPSScoreCalculator.test.ts
@@ -1,6 +1,7 @@
 enum SHAPE {
     ROCK,
-    PAPER
+    PAPER,
+    SCISSORS,
 }
 
 enum OUTCOME {
@@ -16,11 +17,12 @@ class RPSScoreCalculator {
 }
 
 class Round {
-    constructor(private opponentMove: SHAPE, private myMove: SHAPE) {}
+    constructor(private opponentMove: SHAPE, private myMove: SHAPE) {
+    }
 
     outcome(): OUTCOME {
-        if(this.isDraw()) return OUTCOME.DRAW;
-        if(this.isLoss()) return OUTCOME.LOSS;
+        if (this.isDraw()) return OUTCOME.DRAW;
+        if (this.isLoss()) return OUTCOME.LOSS;
         return OUTCOME.WIN;
     }
 
@@ -29,7 +31,9 @@ class Round {
     }
 
     private isLoss() {
-        return this.opponentMove === SHAPE.PAPER && this.myMove === SHAPE.ROCK;
+        return this.opponentMove === SHAPE.PAPER && this.myMove === SHAPE.ROCK
+            || this.opponentMove === SHAPE.ROCK && this.myMove === SHAPE.SCISSORS
+            || this.opponentMove === SHAPE.SCISSORS && this.myMove === SHAPE.PAPER;
     }
 }
 
@@ -54,8 +58,66 @@ class ScoreOutcomeCalculator {
 *  - Avec cette démarche j'implémente deux règles en même temps ( calcul du score de la mache = point de victoire + ce qu'on a joué),
 * est ce qu'il faut décomposé sachant que d'un point de vue externe seul nous interesse le total ?
 * => Ma solution, je créer des classe intermédiaire que je tests, pas sur que ça soit ok
+* => Pareil avec Round que je teste pour avoir l'exhaustivité des cas mais quand je commence les tests j'ai déjà pleins de cas qui sont vert
 * */
 describe("RPSScoreCalculator", () => {
+    describe("Round", () => {
+        describe("Draw", () => {
+            test("Outcome should be draw if Rock and Rock", () => {
+                // GIVEN
+                const round = new Round(SHAPE.ROCK, SHAPE.ROCK);
+                // WHEN
+                const outcome = round.outcome();
+                // THEN
+                expect(outcome).toEqual(OUTCOME.DRAW)
+            })
+
+            test("Outcome should be draw if PAPER and PAPER", () => {
+                // GIVEN
+                const round = new Round(SHAPE.PAPER, SHAPE.PAPER);
+                // WHEN
+                const outcome = round.outcome();
+                // THEN
+                expect(outcome).toEqual(OUTCOME.DRAW)
+            })
+
+            test("Outcome should be draw if SCISSORS and SCISSORS", () => {
+                // GIVEN
+                const round = new Round(SHAPE.SCISSORS, SHAPE.SCISSORS);
+                // WHEN
+                const outcome = round.outcome();
+                // THEN
+                expect(outcome).toEqual(OUTCOME.DRAW)
+            })
+        })
+
+        describe("Loss", () => {
+            test("Outcome should be LOSS if Paper and Rock", () => {
+                // GIVEN
+                const round = new Round(SHAPE.PAPER, SHAPE.ROCK);
+                // WHEN
+                const outcome = round.outcome();
+                // THEN
+                expect(outcome).toEqual(OUTCOME.LOSS)
+            })
+            test("Outcome should be LOSS if Rock and Scissors", () => {
+                // GIVEN
+                const round = new Round(SHAPE.ROCK, SHAPE.SCISSORS);
+                // WHEN
+                const outcome = round.outcome();
+                // THEN
+                expect(outcome).toEqual(OUTCOME.LOSS)
+            })
+            test("Outcome should be LOSS if Scissors and Paper", () => {
+                // GIVEN
+                const round = new Round(SHAPE.SCISSORS, SHAPE.PAPER);
+                // WHEN
+                const outcome = round.outcome();
+                // THEN
+                expect(outcome).toEqual(OUTCOME.LOSS)
+            })
+        })
+    });
     describe("ScoreOutcomeCalculator", () => {
         test("Should return 0 if I lose", () => {
             // GIVEN
