6dba01187a9755426fad261224ec53673f866c22
Clément Dubois <clement.dubois@octo.com>
2022-12-06 20:01:07 +0100

    [DAY 2] - add win calculation score

--- day-2/RPSScoreCalculator.test.ts
+++ day-2/RPSScoreCalculator.test.ts
@@ -3,32 +3,50 @@ enum SHAPE {
     PAPER
 }
 
-class RPSScoreCalculator {
-    count(rounds: Round[]) {
-        return 8;
-    }
-}
-
 enum OUTCOME {
     WIN,
     LOSS,
     DRAW
 }
 
+class RPSScoreCalculator {
+    count(rounds: Round[]) {
+        return 8;
+    }
+}
+
 class Round {
     constructor(private opponentMove: SHAPE, private myMove: SHAPE) {}
 
     outcome(): OUTCOME {
-        return this.opponentMove === this.myMove ? OUTCOME.DRAW : OUTCOME.LOSS
+        if(this.isDraw()) return OUTCOME.DRAW;
+        if(this.isLoss()) return OUTCOME.LOSS;
+        return OUTCOME.WIN;
+    }
+
+    private isDraw() {
+        return this.opponentMove === this.myMove;
+    }
+
+    private isLoss() {
+        return this.opponentMove === SHAPE.PAPER && this.myMove === SHAPE.ROCK;
     }
 }
 
 class ScoreOutcomeCalculator {
+    private static WIN_SCORE = 6;
     private static DRAW_SCORE = 3;
     private static LOSS_SCORE = 0;
 
     count(round: Round) {
-        return round.outcome() === OUTCOME.DRAW ? ScoreOutcomeCalculator.DRAW_SCORE : ScoreOutcomeCalculator.LOSS_SCORE
+        switch (round.outcome()) {
+            case OUTCOME.WIN:
+                return ScoreOutcomeCalculator.WIN_SCORE;
+            case OUTCOME.DRAW:
+                return ScoreOutcomeCalculator.DRAW_SCORE;
+            case OUTCOME.LOSS :
+                return ScoreOutcomeCalculator.LOSS_SCORE;
+        }
     }
 }
 
@@ -42,8 +60,7 @@ describe("RPSScoreCalculator", () => {
         test("Should return 0 if I lose", () => {
             // GIVEN
             const round1 = new Round(SHAPE.PAPER, SHAPE.ROCK);
-            let scoreOutcomeCalculator = new ScoreOutcomeCalculator();
-
+            const scoreOutcomeCalculator = new ScoreOutcomeCalculator();
             // WHEN
             const outcomeScore = scoreOutcomeCalculator.count(round1);
             // THEN
@@ -53,12 +70,21 @@ describe("RPSScoreCalculator", () => {
         test("Should return 3 if draw", () => {
             // GIVEN
             const round1 = new Round(SHAPE.PAPER, SHAPE.PAPER);
-            let scoreOutcomeCalculator = new ScoreOutcomeCalculator();
-
+            const scoreOutcomeCalculator = new ScoreOutcomeCalculator();
             // WHEN
             const outcomeScore = scoreOutcomeCalculator.count(round1);
             // THEN
             expect(outcomeScore).toEqual(3)
+        })
+
+        test("Should return 6 if win", () => {
+            // GIVEN
+            const round1 = new Round(SHAPE.ROCK, SHAPE.PAPER);
+            const scoreOutcomeCalculator = new ScoreOutcomeCalculator();
+            // WHEN
+            const outcomeScore = scoreOutcomeCalculator.count(round1);
+            // THEN
+            expect(outcomeScore).toEqual(6)
         })
     })
     test("Should return 8 for A - Y / Rock - Paper", () => {
