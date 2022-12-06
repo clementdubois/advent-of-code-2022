7987d5510a514f47b07dd4ad00e07523e63d05ca
Clément Dubois <clement.dubois@octo.com>
2022-12-06 17:18:28 +0100

    puzzle input as texte

--- main.ts
+++ main.ts
@@ -1,14 +1,14 @@
+import fs from "fs";
 import {CalorieInputParser} from "./day-1-calorie-counting/src/CalorieInputParser";
 import {CaloriesCounter} from "./day-1-calorie-counting/src/CalorieCounter";
 import {CalorieCountingSolver} from "./day-1-calorie-counting/src/CalorieCountingSolver";
-import {puzzleInput} from "./day-1-calorie-counting/src/calorieCountingInput";
-
 
 /* Day 1 */
 const calorieInputParser = new CalorieInputParser();
 const calorieCounter = new CaloriesCounter();
 const calorieCountingSolver  = new CalorieCountingSolver(calorieInputParser, calorieCounter);
 
-console.log("Day 1 - Part 1 : ", calorieCountingSolver.solve(puzzleInput));
-console.log("Day 1 - Part 2 : ", calorieCountingSolver.solve(puzzleInput, 3));
+const puzzleInput1 = fs.readFileSync("./day-1-calorie-counting/src/calorieCountingInput.txt").toString()
+console.log("Day 1 - Part 1 : ", calorieCountingSolver.solve(puzzleInput1));
+console.log("Day 1 - Part 2 : ", calorieCountingSolver.solve(puzzleInput1, 3));
 
