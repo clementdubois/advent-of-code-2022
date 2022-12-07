10c6ced3a3a40995dad659780f15745d49fd4287
Clément Dubois <clement.dubois@octo.com>
2022-12-07 13:31:14 +0100

    [DAY 2] - parse Y and Z as Paper and Scissors + extract ClassTransformer

--- day-2/RPSInputParser.test.ts
+++ day-2/RPSInputParser.test.ts
@@ -1,4 +1,5 @@
 import {SHAPE} from "./RPSScoreCalculator";
+import {transform} from "@babel/core";
 
 enum OPPONENT_MOVE {
     ROCK = "A",
@@ -8,22 +9,38 @@ enum OPPONENT_MOVE {
 
 enum MY_MOVE {
     ROCK = "X",
+    PAPER = "Y",
+    SCISSORS = "Z",
 }
 
 class RPSInputParser {
+    private inputMoveToRPSTransformer: InputMoveToRPSTransformer;
+    constructor() {
+        this.inputMoveToRPSTransformer = new InputMoveToRPSTransformer();
+    }
     parse(input: string): [SHAPE, SHAPE][] {
-        const [opponentMove, myMove] = RPSInputParser.parseOneRound(input);
-        if (opponentMove) {
-            return [[
-                RPSInputParser.transformOpponentMoveToRPS(opponentMove),
-                RPSInputParser.transformMyMoveToRPS(myMove)
-            ]]
+        const round1 = RPSInputParser.parseOneRound(input);
+        if (round1.length) {
+            return [
+                this.inputMoveToRPSTransformer.transform(round1),
+            ]
         }
         return []
     }
 
     private static parseOneRound(input: string) {
-        return input.split(" ") as [OPPONENT_MOVE, MY_MOVE];
+        return input.split(" ").filter(round => round) as [OPPONENT_MOVE, MY_MOVE];
+    }
+
+
+}
+
+class InputMoveToRPSTransformer {
+    transform(round: [OPPONENT_MOVE, MY_MOVE]):  [SHAPE, SHAPE] {
+        return [
+            InputMoveToRPSTransformer.transformOpponentMoveToRPS(round[0]),
+            InputMoveToRPSTransformer.transformMyMoveToRPS(round[1])
+        ]
     }
 
     private static transformOpponentMoveToRPS(opponentMove: OPPONENT_MOVE): SHAPE {
@@ -39,12 +56,21 @@ class RPSInputParser {
 
     private static transformMyMoveToRPS(myMove: MY_MOVE): SHAPE {
         switch (myMove) {
+            case MY_MOVE.PAPER:
+                return SHAPE.PAPER;
+            case MY_MOVE.SCISSORS:
+                return SHAPE.SCISSORS;
             case MY_MOVE.ROCK:
                 return SHAPE.ROCK;
         }
     }
 }
 
+/* Au début j'avais juste un RPSInputParser, je me rends compte qu'il y a deux responsabilités :
+transformer l'input textuel en tableau et transformer les coups en SHAPE
+=> J'extrais un InputMoveToRPSTransformer, est ce que je refais les tests U, si oui est ce que j'enleve les TU du parser,
+est ce que je suis pas en train de coupler à l'implem, est ce qu'il faut une autre classe qui fasse le relai entre le parser et le transformer ?
+*/
 describe("RPSInputParser", () => {
     let rpsInputParser: RPSInputParser;
 
@@ -87,11 +113,25 @@ describe("RPSInputParser", () => {
     })
     describe("My move", () => {
         test("Should return Rock shape for 'X'", () => {
-            const input = OPPONENT_MOVE.ROCK + " X";
+            const input = OPPONENT_MOVE.ROCK + " " + MY_MOVE.ROCK;
 
             const rounds = rpsInputParser.parse(input)
 
             expect(rounds[0][1]).toEqual(SHAPE.ROCK)
+        })
+        test("Should return Paper shape for 'Y'", () => {
+            const input = OPPONENT_MOVE.ROCK + " " + MY_MOVE.PAPER;
+
+            const rounds = rpsInputParser.parse(input)
+
+            expect(rounds[0][1]).toEqual(SHAPE.PAPER)
+        })
+        test("Should return Scissors shape for 'Z'", () => {
+            const input = OPPONENT_MOVE.ROCK + " " + MY_MOVE.SCISSORS;
+
+            const rounds = rpsInputParser.parse(input)
+
+            expect(rounds[0][1]).toEqual(SHAPE.SCISSORS)
         })
     })
 });
