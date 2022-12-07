ef47c0f5b35e43ade40cb4590ffbf9ab50a087d3
Clément Dubois <clement.dubois@octo.com>
2022-12-07 12:53:06 +0100

    [DAY 2] - parse C as scissors

--- day-2/RPSInputParser.test.ts
+++ day-2/RPSInputParser.test.ts
@@ -4,14 +4,19 @@ class RPSInputParser {
     parse(input: string) {
         if(input === "A") return [[SHAPE.ROCK]];
         if(input === "B") return [[SHAPE.PAPER]];
+        if(input === "C") return [[SHAPE.SCISSORS]]
         return []
     }
 }
 
 describe("RPSInputParser", () => {
+    let rpsInputParser: RPSInputParser;
+
+    beforeEach(() => {
+        rpsInputParser = new RPSInputParser();
+    })
    test("Should return empty array for no input", () => {
        const input = "";
-       const rpsInputParser = new RPSInputParser()
 
        const rounds = rpsInputParser.parse(input)
 
@@ -20,7 +25,6 @@ describe("RPSInputParser", () => {
 
     test("Should return Rock shape for 'A'", () => {
         const input = "A";
-        const rpsInputParser = new RPSInputParser()
 
         const rounds = rpsInputParser.parse(input)
 
@@ -29,10 +33,17 @@ describe("RPSInputParser", () => {
 
     test("Should return Paper shape for 'B'", () => {
         const input = "B";
-        const rpsInputParser = new RPSInputParser()
 
         const rounds = rpsInputParser.parse(input)
 
         expect(rounds[0][0]).toEqual(SHAPE.PAPER)
+    })
+
+    test("Should return Scissors shape for 'C'", () => {
+        const input = "C";
+
+        const rounds = rpsInputParser.parse(input)
+
+        expect(rounds[0][0]).toEqual(SHAPE.SCISSORS)
     })
 });
