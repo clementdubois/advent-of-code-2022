45918ff5823e033e5feba5b4019acfbbb4fcac05
Clément Dubois <clement.dubois@octo.com>
2022-12-07 13:07:58 +0100

    [DAY 2] - refactoring

--- day-2/RPSInputParser.test.ts
+++ day-2/RPSInputParser.test.ts
@@ -1,11 +1,30 @@
 import {SHAPE} from "./RPSScoreCalculator";
 
+enum OPPONENT_MOVE {
+    ROCK = "A",
+    PAPER = "B",
+    SCISSORS = 'C'
+}
+
 class RPSInputParser {
     parse(input: string) {
-        if(input === "A") return [[SHAPE.ROCK]];
-        if(input === "B") return [[SHAPE.PAPER]];
-        if(input === "C") return [[SHAPE.SCISSORS]]
-        return []
+        const [opponentMove] = RPSInputParser.parseOneRound(input);
+        return opponentMove ? [[RPSInputParser.transformOpponentMoveToRPS(opponentMove)]] : []
+    }
+
+    private static parseOneRound(input: string) {
+        return input.split(" ") as [OPPONENT_MOVE];
+    }
+
+    private static transformOpponentMoveToRPS(opponentMove: OPPONENT_MOVE): SHAPE {
+        switch (opponentMove) {
+            case OPPONENT_MOVE.ROCK:
+                return SHAPE.ROCK;
+            case OPPONENT_MOVE.PAPER:
+                return SHAPE.PAPER;
+            case OPPONENT_MOVE.SCISSORS:
+                return SHAPE.SCISSORS;
+        }
     }
 }
 
@@ -15,35 +34,47 @@ describe("RPSInputParser", () => {
     beforeEach(() => {
         rpsInputParser = new RPSInputParser();
     })
-   test("Should return empty array for no input", () => {
-       const input = "";
+    describe("Opponent move", () => {
 
-       const rounds = rpsInputParser.parse(input)
+        test("Should return empty array for no input", () => {
+            const input = "";
 
-       expect(rounds).toEqual([])
-   })
+            const rounds = rpsInputParser.parse(input)
 
-    test("Should return Rock shape for 'A'", () => {
-        const input = "A";
+            expect(rounds).toEqual([])
+        })
 
-        const rounds = rpsInputParser.parse(input)
+        test("Should return Rock shape for 'A'", () => {
+            const input = OPPONENT_MOVE.ROCK;
 
-        expect(rounds[0][0]).toEqual(SHAPE.ROCK)
+            const rounds = rpsInputParser.parse(input)
+
+            expect(rounds[0][0]).toEqual(SHAPE.ROCK)
+        })
+
+        test("Should return Paper shape for 'B'", () => {
+            const input = OPPONENT_MOVE.PAPER;
+
+            const rounds = rpsInputParser.parse(input)
+
+            expect(rounds[0][0]).toEqual(SHAPE.PAPER)
+        })
+
+        test("Should return Scissors shape for 'C'", () => {
+            const input = OPPONENT_MOVE.SCISSORS;
+
+            const rounds = rpsInputParser.parse(input)
+
+            expect(rounds[0][0]).toEqual(SHAPE.SCISSORS)
+        })
     })
+    describe.skip("My move", () => {
+        test("Should return Rock shape for 'X'", () => {
+            const input = OPPONENT_MOVE.ROCK + " X";
 
-    test("Should return Paper shape for 'B'", () => {
-        const input = "B";
+            const rounds = rpsInputParser.parse(input)
 
-        const rounds = rpsInputParser.parse(input)
-
-        expect(rounds[0][0]).toEqual(SHAPE.PAPER)
-    })
-
-    test("Should return Scissors shape for 'C'", () => {
-        const input = "C";
-
-        const rounds = rpsInputParser.parse(input)
-
-        expect(rounds[0][0]).toEqual(SHAPE.SCISSORS)
+            expect(rounds[0][1]).toEqual(SHAPE.ROCK)
+        })
     })
 });
